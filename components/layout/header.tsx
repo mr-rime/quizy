d9c6331b5492e63af8df203389931046cba4e7c6
rimex3 <faresahmed51857@gmail.com>
2025-12-03 22:15:52 +0200

    feat: Installed shadcn and implemented header and dark mode with theme toggle

---
+++ components/layout/header.tsx
@@ -0,0 +1,13 @@
+import { ThemeToggle } from "../theme-toggle";
+
+export function Header() {
+    return (
+        <header className="p-2 h-18 flex items-center justify-between px-20 border-b">
+            <h1 className="text-black dark:text-white text-2xl">
+                <span className="font-bold">Q</span>uizy
+            </h1>
+
+            <ThemeToggle />
+        </header>
+    )
+}
